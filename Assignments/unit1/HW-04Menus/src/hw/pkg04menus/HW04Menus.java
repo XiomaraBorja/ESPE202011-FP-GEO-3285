b11e2cef0f62219be04c07f62148a946685d0aaa
XiomaraBorja <xiomaraarielbp@gmail.com>
2021-01-17 22:35:55 -0500

    HW-04Menus

--- Assignments/unit1/HW-04Menus/src/hw/pkg04menus/HW04Menus.java
+++ Assignments/unit1/HW-04Menus/src/hw/pkg04menus/HW04Menus.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hw04menus;
+package hw.pkg04menus;
 
 import java.util.Scanner;
 
@@ -18,8 +18,7 @@ public class HW04Menus {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
     boolean  mainLoop = true;
     int option;
         
@@ -27,18 +26,32 @@ Scanner input = new Scanner(System.in);
     do{
    
         System.out.println("calculator");
-        System.out.println("1. -> trapezium area ");
-        System.out.println("2. ->addition");
+        System.out.println("1. -> addition ");
+        System.out.println("2. -> trapezium area ");
         System.out.println("3. -> Cylinder volume ");
-        System.out.println("4. -> Cylinder area ");
-        System.out.println("5. ->Exit");
+       System.out.println("4. -> Subtraction"); 
+       System.out.println("5. ->Exit");
         
         System.out.println("1. ->Enter your menu option -->");
         option = input.nextInt();
         switch(option){
                 
-            case 1:      
+            case 1:     
+
+int addend1;
+                int addend2;
+                int sum;
+                System.out.println("enter addend 1->");
+                addend1 =input.nextInt();
+                System.out.println("enter addend 2 ->");
+                addend2 = input.nextInt();
+                sum = addend1 + addend2;
+               System.out.println("the addition is --> " + sum);
+               break;
+ 
       
+            case 2:
+
      Scanner in = new Scanner(System.in);
    int B=0,b=0,h=0,A=0;
    System.out.println("Ingrese el valor de la base mayor del trapecio");
@@ -50,17 +63,8 @@ Scanner input = new Scanner(System.in);
    A=((B+b)/2)*h;
    System.out.println("El valor de area del trapecio es " +A);
     break;
-            case 2:
-                int addend1;
-                int addend2;
-                int sum;
-                System.out.println("enter addend 1->");
-                addend1 =input.nextInt();
-                System.out.println("enter addend 2 ->");
-                addend2 = input.nextInt();
-                sum = addend1 + addend2;
-               System.out.println("the addition is --> " + sum);
-               break;
+
+                
            
             case 3:
                 
@@ -76,17 +80,9 @@ Scanner input = new Scanner(System.in);
         System.out.println("El volumen de un cilindro -> " + vcili + "cms3");
         break;
             case 4:   
-       int r1;
-        int H1;
-        int atotal;
-        System.out.println("ingrese el radio del cilindro : ");
-      r1 = input.nextInt();
-        System.out.println("Ingrese la altura del cilindro");
-        H1 = input.nextInt();
-        atotal = (int) (((2)* (3.1416) * (r1)) * (H1 +r1));
-        System.out.println("El area de un cilindro -> " + atotal + "cms2");
-        break;
-        
+
+                    System.out.println("The subtraction ");
+                    break;        
             case 5 :
          default:
                System.out.println(" Invelid option \n\n\n ");
@@ -95,4 +91,7 @@ Scanner input = new Scanner(System.in);
         }
        }while (option >5);
     }
-}
+
+    }
+    
+
